030a669f9c2ae9e05b3a3fe9a16042530bac2d78
May Lawver <may@maycod.es>
2019-09-07 22:18:57 -0400

    Added CombinationEffect.

--- _source/AbstractEffect.ts
+++ _source/AbstractEffect.ts
@@ -1,10 +1,26 @@
 abstract class AbstractEffect {
   next: AbstractEffect | null;
-  abstract activate(user: Combatant, target: Combatant): void;
+  abstract activate(user: Combatant, foe: Combatant): void;
 }
 
 class NothingEffect extends AbstractEffect { //does nothing
-  activate(user: Combatant, target: Combatant): void {
+  activate(user: Combatant, foe: Combatant): void {
     return;
   }
+}
+
+class CombinationEffect extends AbstractEffect { //combine multiple effects
+
+  effects: AbstractEffect[];
+
+  constructor(...effects: AbstractEffect[]) {
+    this.effects = effects;
+  }
+
+  activate(user: Combatant, foe: Combatant): void {
+    for (let i = 0; i < this.effects.length; i++) {
+      this.effects[i].activate(user, foe);
+    }
+  }
+
 }
