a300f3ed1ec02b2c82283752b36a2b466c3e0f18
May Lawver <may@maycod.es>
2019-09-07 21:53:55 -0400

    Added NothingEffect.

--- _source/AbstractEffect.ts
+++ _source/AbstractEffect.ts
@@ -1,3 +1,10 @@
 abstract class AbstractEffect {
-  
+  next: AbstractEffect | null;
+  abstract activate(user: Combatant, target: Combatant): void;
+}
+
+class NothingEffect extends AbstractEffect { //does nothing
+  activate(user: Combatant, target: Combatant): void {
+    return;
+  }
 }
