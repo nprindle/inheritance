dfed51bf0a4546c5701fe283007bdc989d46b4cf
May Lawver <may@maycod.es>
2019-09-07 22:19:46 -0400

    AbstractActions have a next.

--- _source/AbstractAction.ts
+++ _source/AbstractAction.ts
@@ -1,5 +1,13 @@
 /// <reference path="AbstractEffect.ts" />
 
 abstract class AbstractAction {
+
+  next: AbstractAction | null;
+
+  constructor(next: AbstractAction | null) {
+    this.next = next;
+  }
+
   abstract activate(): AbstractEffect;
+
 }
