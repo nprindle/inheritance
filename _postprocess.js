8acc79a8d67572b7fef2efb1c862405aac8f2ee8
May Lawver <may@maycod.es>
2019-09-20 16:44:30 -0400

    Postprocessor adds newlines.

--- _postprocess.js
+++ _postprocess.js
@@ -9,11 +9,11 @@ window = {
 };
 require('./built.js');
 const fs = require('fs');
-const toolLines = ['# Tools'];
+const toolLines = ['# Tools\n'];
 for (let k in processor.tools.items) {
   tool = processor.tools.items[k];
-  toolLines.push(`## ${tool._name}`);
-  toolLines.push(`Cost: ${tool.cost.toString()}`);
-  toolLines.push(`${tool.effectsString()}`);
+  toolLines.push(`## ${tool._name}\n`);
+  toolLines.push(`Cost: ${tool.cost.toString()}\n`);
+  toolLines.push(`${tool.effectsString()}\n`);
 }
 fs.writeFileSync('_tools.md', toolLines.join('\n'));
--- _tools.md
+++ _tools.md
@@ -1,10 +1,19 @@
 # Tools
+
 ## Bandages
+
 Cost: 1 Energy
+
 Recover 1 health.
+
 ## Singleton
+
 Cost: 1 Energy
+
 Do 5 damage.
+
 ## Wrench
+
 Cost: 1 Energy
+
 Do 1 damage.
