41b10fa021d924750f8e2597d5862dc514a81627
May Lawver <may@maycod.es>
2019-09-20 15:16:10 -0400

    You can append arbitrary lines to the preprocessed code.

--- _preprocess.js
+++ _preprocess.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
-function makeDirectoryReferences(dir) {
+function makeDirectoryReferences(dir, ...lines) {
   let fulldir = `_source/${dir}`;
   let files = fs.readdirSync(fulldir);
   files = files.filter(x => x.endsWith('.ts'));
-  let lines = files.map(x => `/// <reference path="${dir}/${x}" />`);
+  lines = lines.concat(files.map(x => `/// <reference path="${dir}/${x}" />`));
   let file = lines.join('\n');
   fs.writeFileSync(fulldir + '.ts', file + '\n');
 }
