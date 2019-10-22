b6e77259ed92d616c9c480f76245a73354b2c7e3
May Lawver <may@maycod.es>
2019-10-22 18:10:08 -0400

    Buffed Lighter.

--- _source/tools/Lighter.ts
+++ _source/tools/Lighter.ts
@@ -4,7 +4,7 @@
 /// <reference path="../statuses.ts" />
 tools.add('lighter',
   new Tool('Lighter', new Cost([1, CostTypes.Energy]),
-    new GiveSelfStatusEffect(new BurnStatus(2)),
+    new GiveSelfStatusEffect(new BurnStatus(1)),
     new GiveSelfStatusEffect(new StrengthStatus(1))
   )
 );
