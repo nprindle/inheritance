ffe8117377acfc2781790cd8955c8ec189693a41
May Lawver <may@maycod.es>
2019-10-25 04:24:03 -0400

    Added more items to the Granddaughter's loadout.

--- _source/characters/TheGranddaughter.ts
+++ _source/characters/TheGranddaughter.ts
@@ -5,5 +5,7 @@
 characters.addSorted('kid', new Player('The Granddaughter', 15, 10,
     tools.get('wrench')!,
     tools.get('poisonray')!,
-    tools.get('lighter')!
+    tools.get('lighter')!,
+    tools.get('jacket')!,
+    tools.get('thumbtack')!
 ), 0);
