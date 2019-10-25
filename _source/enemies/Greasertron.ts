18b6c1ef905a9fcc9cc54e023cc1892519b72552
May Lawver <may@maycod.es>
2019-10-25 04:12:08 -0400

    Added Greasertron.

---
+++ _source/enemies/Greasertron.ts
@@ -0,0 +1,8 @@
+/// <reference path="../ItemPool.ts" />
+/// <reference path="../tools.ts" />
+/// <reference path="../Enemy.ts" />
+/// <reference path="../AiUtilityFunctions.ts" />
+
+enemies.add('greasertron',
+    new Enemy('Greaser-Tron', 10, 6, AiUtilityFunctions.aggressiveUtility, tools.get('lighter')!, tools.get('switchblade')!, tools.get('comb')!),
+EnemyTags.level1);
