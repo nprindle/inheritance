0e87ff81d15eaab856ab2227080859e4b171489a
May Lawver <may@maycod.es>
2019-10-29 03:27:31 -0400

    Added Fungal status.

---
+++ _source/statuses/FungalStatus.ts
@@ -0,0 +1,42 @@
+/// <reference path="../AbstractStatus.ts" />
+/// <reference path="PoisonStatus.ts" />
+
+class FungalStatus extends AbstractStatus {
+
+    constructor(amount: number) {
+        super(amount);
+    }
+
+    takeDamage(affected: Combatant, other: Combatant) {
+        other.addStatus(new PoisonStatus(this.amount));
+    }
+
+    add(other: AbstractStatus): boolean {
+        if (other instanceof FungalStatus) {
+            this.amount += other.amount;
+            return true;
+        }
+        return false;
+    }
+
+    clone(): FungalStatus {
+        return new FungalStatus(this.amount);
+    }
+
+    getName(): string {
+        return 'fungal';
+    }
+
+    getDescription(): string {
+        return `Opponent gains ${this.amount} poison whenever you take damage.`;
+    }
+
+    getSortingNumber(): number {
+        return 0;
+    }
+
+    getUtility(): number {
+        return this.amount;
+    }
+
+}
