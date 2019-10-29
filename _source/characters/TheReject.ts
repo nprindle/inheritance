38dfd9442b7c25bd801bb3db6b246e505038150c
May Lawver <may@maycod.es>
2019-10-29 05:33:55 -0400

    Reduced Reject's energy amount.

--- _source/characters/TheReject.ts
+++ _source/characters/TheReject.ts
@@ -2,7 +2,7 @@
 /// <reference path="../ItemPool.ts" />
 /// <reference path="../tools.ts" />
 
-characters.addSorted('reject', new Player('The Reject', 10, 10,
+characters.addSorted('reject', new Player('The Reject', 10, 4,
     tools.get('mutagens')!,
     tools.get('boneclaws')!,
     tools.get('rawhideskin')!,
