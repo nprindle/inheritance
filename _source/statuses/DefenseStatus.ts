382ff3ccfd3d435473fefc830a5826cd0d7903d2
May Lawver <may@maycod.es>
2019-10-29 05:47:01 -0400

    Added DefenseStatus.

---
+++ _source/statuses/DefenseStatus.ts
@@ -0,0 +1,49 @@
+/// <reference path="../AbstractStatus.ts" />
+
+class DefenseStatus extends AbstractStatus {
+
+    constructor(amount: number) {
+        super(amount);
+    }
+
+    damageTakenFold(acc: number): number {
+        return Math.max(0, acc + this.amount);
+    }
+
+    add(other: AbstractStatus): boolean {
+        if (other instanceof DefenseStatus) {
+            this.amount += other.amount;
+            return true;
+        }
+        return false;
+    }
+
+    sameKind(other: AbstractStatus): boolean {
+        return other instanceof DefenseStatus;
+    }
+
+    clone(): DefenseStatus {
+        return new DefenseStatus(this.amount);
+    }
+
+    getName(): string {
+        return 'defense';
+    }
+
+    getDescription(): string {
+        if (this.amount > 0) {
+            return `Take ${this.amount} less damage from attacks.`;
+        } else {
+            return `Take ${this.amount} more damage from attacks.`;
+        }
+    }
+
+    getSortingNumber(): number {
+        return 0;
+    }
+
+    getUtility(): number {
+        return 2 * this.amount;
+    }
+
+}
