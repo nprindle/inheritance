b9f5a85fdfb8b88d4dd02cd35043c833217b6602
Grace Rarer <gracerarer@gmail.com>
2019-10-30 17:34:12 -0400

    added methods for changing which notes are unlocked

--- _source/NotePool.ts
+++ _source/NotePool.ts
@@ -27,4 +27,37 @@ class NotePool {
             return next;
         }
     }
+
+    // returns a list of the IDs for the unlocked notes, for serializing progress
+    static getUnlockedNoteIDs() {
+        let ids: number[] = [];
+
+        NotePool.getUnlockedNotes().forEach(element => {
+            ids.push(element.id);
+        });
+
+        return ids;
+    }
+
+    // sets which notes are unlocked
+    // use this for deserializing saved games
+    static setUnlockedNotes(unlockedIDs: number[]) {
+        
+        NotePool.notes.forEach(note => {
+            let currentID: number = note.id;
+            // each note is unlocked only if its ID is contained in the list of unlocked note IDs
+            if (unlockedIDs.indexOf(currentID) > -1) {
+                note.unlocked = true;
+            } else {
+                note.unlocked = false;
+            }
+        });
+    }
+
+    // unlock a specific note by title
+    static unlockSpecificNote(title: string) {
+        NotePool.notes.filter(note => (note.title == title)).forEach(note => {
+            note.unlocked = true;
+        });
+    }
 }
