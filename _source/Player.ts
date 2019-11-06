eef25e9e261c5e3915908c34e4fa694f9b0f257a
May Lawver <may@maycod.es>
2019-11-06 17:41:54 -0500

    Players have a currency field.

--- _source/Player.ts
+++ _source/Player.ts
@@ -1,15 +1,23 @@
 ///<reference path="./Combatant.ts" />
 
 class Player extends Combatant {
+    
+    currency: number;
 
     constructor(name: string, health: number, energy: number, ...others: (Tool | Trait)[]) {
         super(name, health, energy, ...others);
+        this.currency = 0;
+    }
+
+    giveCurrency(x: number): void {
+        this.currency += x;
     }
 
     clone(): Player {
         let p = new Player(this.name, this.health, this.energy, ...this.tools.map(x => x.clone()));
         p.statuses = this.statuses.map(x => x.clone());
         p.traits = this.traits.map(x => x.clone());
+        p.currency = this.currency;
         return p;
     }
 
