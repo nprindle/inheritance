43fc0ddcd1d57d102d6edd3d1a67671de9888d7b
May Lawver <may@maycod.es>
2019-11-06 18:04:53 -0500

    Fixed Trait's formatting.

--- _source/Trait.ts
+++ _source/Trait.ts
@@ -3,31 +3,31 @@
 
 class Trait {
 
-  name: string;
-  appliedStatuses: AbstractStatus[];
+    name: string;
+    appliedStatuses: AbstractStatus[];
 
-  constructor(name: string, ...statuses: AbstractStatus[]) {
-    this.name = name;
-    this.appliedStatuses = statuses;
-  }
+    constructor(name: string, ...statuses: AbstractStatus[]) {
+        this.name = name;
+        this.appliedStatuses = statuses;
+    }
 
-  startFight(c: Combatant): void {
-    const statusClones = this.appliedStatuses.map(x => x.clone());
-    statusClones.forEach(status => c.addStatus(status));
-  }
+    startFight(c: Combatant): void {
+        const statusClones = this.appliedStatuses.map(x => x.clone());
+        statusClones.forEach(status => c.addStatus(status));
+    }
 
-  removeEffects(c: Combatant): void {
-    const statusClones = this.appliedStatuses.map(x => x.clone());
-    statusClones.forEach(status => status.amount *= -1);
-    statusClones.forEach(status => c.addStatus(status));
-  }
+    removeEffects(c: Combatant): void {
+        const statusClones = this.appliedStatuses.map(x => x.clone());
+        statusClones.forEach(status => status.amount *= -1);
+        statusClones.forEach(status => c.addStatus(status));
+    }
 
-  clone(): Trait {
-    return new Trait(this.name, ...this.appliedStatuses.map(x => x.clone()));
-  }
+    clone(): Trait {
+        return new Trait(this.name, ...this.appliedStatuses.map(x => x.clone()));
+    }
 
-  describe(): string {
-    return `Start fights with ${this.appliedStatuses.map(x => x.toString()).join(', ')}`;
-  }
+    describe(): string {
+        return `Start fights with ${this.appliedStatuses.map(x => x.toString()).join(', ')}`;
+    }
 
 }
