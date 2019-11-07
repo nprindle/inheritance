2a3efea9ce8070e119fff4c2d885fdbebad5e569
May Lawver <may@maycod.es>
2019-11-07 04:49:23 -0500

    Status predicates only take one argument now.

--- _source/predicates/TargetStatusPredicate.ts
+++ _source/predicates/TargetStatusPredicate.ts
@@ -2,24 +2,22 @@
 
 class TargetStatusPredicate extends AbstractCombatPredicate {
 
-    threshold: number;
     kind: AbstractStatus;
 
-    constructor(threshold: number, kind: AbstractStatus) {
+    constructor(kind: AbstractStatus) {
         super();
-        this.threshold = threshold;
         this.kind = kind;
     }
 
     evaluate(user: Combatant, target: Combatant): boolean {
-        return target.getStatusAmount(this.kind) >= this.threshold;
+        return target.getStatusAmount(this.kind) >= this.kind.amount;
     }
 
     toString(): string {
-        if (this.threshold === 1) {
+        if (this.kind.amount === 1) {
             return `if your opponent has any ${this.kind.getName()}`;
         }
-        return `if your opponent has at least ${this.threshold} ${this.kind.getName()}`;
+        return `if your opponent has at least ${this.kind.amount} ${this.kind.getName()}`;
     }
 
 }
--- _source/predicates/UserStatusPredicate.ts
+++ _source/predicates/UserStatusPredicate.ts
@@ -2,24 +2,22 @@
 
 class UserStatusPredicate extends AbstractCombatPredicate {
 
-    threshold: number;
     kind: AbstractStatus;
 
     constructor(threshold: number, kind: AbstractStatus) {
         super();
-        this.threshold = threshold;
         this.kind = kind;
     }
 
     evaluate(user: Combatant, target: Combatant): boolean {
-        return user.getStatusAmount(this.kind) >= this.threshold;
+        return user.getStatusAmount(this.kind) >= this.kind.amount;
     }
 
     toString(): string {
-        if (this.threshold === 1) {
+        if (this.kind.amount === 1) {
             return `if you have any ${this.kind.getName()}`;
         }
-        return `if you have at least ${this.threshold} ${this.kind.getName()}`;
+        return `if you have at least ${this.kind.amount} ${this.kind.getName()}`;
     }
 
 }
