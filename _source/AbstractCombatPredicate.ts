35f0b89864b1fd653ee59624f73b82039056252b
May Lawver <may@maycod.es>
2019-11-07 04:09:45 -0500

    Added Predicates.

---
+++ _source/AbstractCombatPredicate.ts
@@ -0,0 +1,7 @@
+/// <reference path="Combatant.ts" />
+abstract class AbstractCombatPredicate {
+    
+    abstract toString(): string;
+    abstract evaluate(target: Combatant, other: Combatant): boolean;
+
+}
