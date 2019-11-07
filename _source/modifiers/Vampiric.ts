dd34c3113e4d40314c8516ff00ae031456f414ca
May Lawver <may@maycod.es>
2019-11-06 23:45:26 -0500

    Added Vampiric modifier.

---
+++ _source/modifiers/Vampiric.ts
@@ -0,0 +1,6 @@
+/// <reference path="../ItemPool.ts" />
+/// <reference path="../Modifier.ts" />
+/// <reference path="../effects.ts" />
+modifiers.add('vampiric',
+    new Modifier('Vampiric', [ModifierTypes.EnergyToHealth, 0])
+);
