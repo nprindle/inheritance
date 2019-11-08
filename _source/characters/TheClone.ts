3149c50fc00c9226e03cc41c16260e1984e9cfc8
Justin Prindle <justindprindle@gmail.com>
2019-11-08 18:32:40 -0500

    Remove unnecessary type union varargs
    Use of type-union varargs allows creation of new instances of Combatants
    to be slightly prettier at the expense of requiring extra traversals and
    type inspection in order to separate them out, which isn't extensible.
    Often, we clearly already have distinct sets of tools/traits which have
    to be merged, only to be unmerged later.

--- _source/characters/TheClone.ts
+++ _source/characters/TheClone.ts
@@ -3,8 +3,11 @@
 /// <reference path="../tools.ts" />
 
 characters.addSorted('clone', new Player('The Clone', 10, 10,
-    tools.get('windupraygun')!,
-    tools.get('confusionray')!,
-    tools.get('thermocouple')!,
-    tools.get('energizer')!,
+    [
+        tools.get('windupraygun')!,
+        tools.get('confusionray')!,
+        tools.get('thermocouple')!,
+        tools.get('energizer')!,
+    ],
+    []
 ), 1);
