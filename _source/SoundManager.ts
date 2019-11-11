3e31946d57d0e05955b3a987e559b50bd0e96c05
May Lawver <may@maycod.es>
2019-11-10 21:26:32 -0500

    Sound effects theoretically work.

---
+++ _source/SoundManager.ts
@@ -0,0 +1,27 @@
+enum SoundEffects {
+    Noise = 'noise.ogg',
+}
+
+class SoundManager {
+
+    //TODO: make indexing better
+    static audioElems: Record<string, HTMLAudioElement>;
+
+    static init(): void {
+        this.audioElems = {};
+        let container: HTMLElement = document.getElementById('audio');
+        Object.keys(SoundEffects).forEach(key => {
+            let filename: string = `assets/sfx/${SoundEffects[key]}`;
+            let audio: HTMLAudioElement = document.createElement('audio');
+            audio.src = filename;
+            this.audioElems[SoundEffects[key]] = audio;
+            container.appendChild(audio);
+        });
+    }
+
+    static playSoundEffect(sfx: SoundEffects): void {
+        this.audioElems[sfx].currentTime = 0;
+        this.audioElems[sfx].play();
+    }
+
+}
