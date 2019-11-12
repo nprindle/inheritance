723e0744641f495e4ebd30ff13bd62ad49cd6b96
May Lawver <may@maycod.es>
2019-11-12 01:03:01 -0500

    Stubbed out Catburglar.

---
+++ _source/characters/TheCatburglar.ts
@@ -0,0 +1,13 @@
+/// <reference path="../Player.ts" />
+/// <reference path="../ItemPool.ts" />
+/// <reference path="../tools.ts" />
+/// <reference path="../traits.ts" />
+
+//TODO: implement Flow status and make Catburglar interesting
+
+characters.addSorted('catburglar', new Player('The Catburglar', 10, 10,
+    [
+        tools.get('wrench')!
+    ],
+    [traits.get('counterfeiting')!]
+), 1);
