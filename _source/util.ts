df8eadb4affbef0aab700a880cb1dae6c76dbafa
Justin Prindle <justindprindle@gmail.com>
2019-11-17 17:56:22 -0500

    Allow covariant return types for overriden functions

--- _source/util.ts
+++ _source/util.ts
@@ -1,8 +1,9 @@
 // Enforces that a method is overriden from the superclass
 // Used as an annotation: @override(superclass)
 const override = <S>(superclass: { prototype: S }) =>
-    <K extends keyof S, P extends Pick<S, K>>(
-        proto: P,
+    <K extends keyof S, C extends S[K]>(
+        proto: Pick<S, K>,
         fields: K,
-        descr: TypedPropertyDescriptor<S[K]>,
+        descr: TypedPropertyDescriptor<C>,
     ) => { }
+
