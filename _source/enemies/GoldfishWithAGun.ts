a21c3e32ee2b22e234b514e2b61c61f687754407
May Lawver <may@maycod.es>
2019-11-19 21:40:45 -0500

    Renamed Goldfish with a gun and made a proper goldfish.

--- _source/enemies/GoldfishWithAGun.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../ItemPool.ts" />
-/// <reference path="../tools.ts" />
-/// <reference path="../Enemy.ts" />
-/// <reference path="../AiUtilityFunctions.ts" />
-
-enemies.add('goldfishwithagun',
-    new Enemy('Goldfish With A Gun', 10, 5, AiUtilityFunctions.aggressiveUtility, [tools.get('sixshooter')!], []),
-EnemyTags.level1);
