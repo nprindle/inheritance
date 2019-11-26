906ae7254a4692440a296ffd723e9e3edd6eb8bc
Justin Prindle <justindprindle@gmail.com>
2019-11-25 23:55:32 -0500

    Parameterize scripts with args instead of assuming directory names

--- scripts/before_deploy/build_demo.sh
+++ scripts/before_deploy/build_demo.sh
@@ -1,20 +1,34 @@
+target="$1"
+
+if [ -z "$target" ]; then
+  >&2 echo "Error: no target directory provided"
+  exit 1
+fi
+
+pages_branch="$2"
+
+if [ -z "$pages_branch" ]; then
+  >&2 echo "Error: no GitHub pages branch name provided"
+  exit 1
+fi
+
 # Attempt to check out 'demo' from gh-pages
-git fetch origin gh-pages
+git fetch origin $pages_branch
 git checkout FETCH_HEAD -- demo/
 
 # If it existed and succeeds, copy it into 'dist' to be deployed
 if [ $? -eq 0 ]; then
-  mv demo dist
+  mv demo "$target"
 else
   git checkout refs/tags/demo
 
   npm run build-all
 
-  mkdir -p dist/demo
-  cp -R assets dist/demo/
-  cp built.js dist/demo/
-  cp *.html dist/demo/
-  cp *.css dist/demo/
+  mkdir -p "$target/demo"
+  cp -R assets "$target/demo/"
+  cp built.js "$target/demo/"
+  cp *.html "$target/demo/"
+  cp *.css "$target/demo/"
 
   git checkout -
 fi
--- scripts/before_deploy/make_dist.sh
+++ scripts/before_deploy/make_dist.sh
@@ -1,5 +1,13 @@
-mkdir dist
-cp -R assets dist/
-cp built.js dist/
-cp *.html dist/
-cp *.css dist/
+# The directory to move necessary files into
+target="$1"
+
+if [ -z "$target" ]; then
+  >&2 echo "Error: no target directory provided"
+  exit 1
+fi
+
+mkdir "$1"
+cp -R assets "$1"/
+cp built.js "$1"
+cp *.html "$1"
+cp *.css "$1"
