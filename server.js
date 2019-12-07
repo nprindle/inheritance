e458b431dc1ca9ef58723c79b36bb2f997b0ae54
Justin Prindle <justindprindle@gmail.com>
2019-12-07 16:34:26 -0500

    Switch to using live-server for local server

--- server.js
+++
@@ -1,9 +0,0 @@
-let connect = require('connect');
-let serveStatic = require('serve-static');
-
-const PORT = isNaN(process.env.PORT) ? 8080 : parseInt(process.env.PORT);
-
-connect().use(serveStatic(__dirname)).listen(PORT, () => {
-  console.log(`Server running on port ${PORT}...`);
-})
-
